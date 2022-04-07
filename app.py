76c2e8dd54fc5da02d5d7d070c160f949bf326e3
Owen-Teo <2101612@sit.singaporetech.edu.sg>
2022-04-07 12:50:05 +0800

    basic button

--- app.py
+++ app.py
@@ -45,48 +45,9 @@ def setup():
 setup()
 @app.route("/")
 def home():
-    return render_template('index.html')
-
-@app.route("/Register")
-def Register():
-    return render_template('Register.html')
-
-@app.route("/signin", methods=['POST'])
-def signin():
-    username = request.form['username']
-    password = request.form['password']
-    if username and password:
-        if validateUser(username, password):
-            print("Log in as " + username + ", " + password)
-            return render_template('Main.html')
+    return render_template('main.html')
 
 
-@app.route("/register", methods=['POST'])
-def register():
-    print("this is a test")
-    username = request.form['username']
-    password = request.form['password']
-    cpassword = request.form['Cpassword']
-    if request.method == 'POST' and cpassword == password:
-        with open('customer.txt', 'a') as f:
-            print("writing")
-            txt1 = "{fname}:{password}:".format(fname=username, password=password)
-            f.write(txt1)
-
-    return render_template('index.html')
-
-def validateUser(username, password):
-    f = open('customer.txt')
-    l = []
-    i = 0
-    for line in f:
-        l = line.split(":")
-        if username == l[i] and password == l[i + 1]:
-            return True
-        i += 2
-
-    return False
-    
 
 def book(user_id ,start, end ,cartype,seats = 1 , luggage_req =0 , sharing = False ,distance_limit = 300):
     global journey_No
